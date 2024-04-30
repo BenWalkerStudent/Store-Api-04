b7169d0589acc28d50cdd71d30ecb2a3428e5bd3
Benjamin Walker <bwalker33@SD-0404.local>
2024-04-30 13:55:10 -0600

    main

---
+++ controllers/products.js
@@ -0,0 +1,56 @@
+const Product = require('../models/product')
+
+const getAllProductsStatic = async (req,res) => {
+    const search = 'a'
+    const products = await Product.find({}).select('name price')
+
+    res.status(200).json({ products, nbHits:products.length})
+
+}
+
+const getAllProducts = async (req,res) => {
+    const { featured,company, name, sort, fields} = req.query;
+    const queryObject = {};
+
+    if (featured) {
+
+        queryObject.featured = featured === 'true'? true : false;
+
+    }
+    if(company){
+
+        queryObject.company = company
+
+    }
+    if(name) {
+
+        queryObject.name = { $regex:name, $options: 'i' }
+
+    }
+    //console.log(queryObject)
+    let result = Product.find(queryObject);
+  // sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ');
+    result = result.sort(sortList);
+  } else {
+    result = result.sort('createdAt');
+  }
+
+    if (fields) {
+        const fieldsList = fields.split(',').join(' ');
+        result = result.select(fieldsList);
+      }
+
+    const products = await  result
+
+    res.status(200).json({ products, nbHits: products.length})
+
+}
+
+module.exports = {
+
+    getAllProducts,
+    getAllProductsStatic
+
+}
